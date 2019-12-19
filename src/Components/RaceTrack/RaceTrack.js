b45a13c0ac72b335cc4dc27bfd39990c971b7247
Jake M <mullerjp95@gmail.com>
2019-12-19 15:12:06 -0500

    added dependencies for backend

--- src/Components/RaceTrack/RaceTrack.js
+++ src/Components/RaceTrack/RaceTrack.js
@@ -8,12 +8,13 @@ export default function RaceTrack(props) {
     <div className="track-container">
         <img
           className="racer"
-          src="http://pixelartmaker.com/art/0450984eb0e24b5.png"
+          // src="http://pixelartmaker.com/art/0450984eb0e24b5.png"
+          src='https://media.discordapp.net/attachments/409463862098919424/657054163720601600/unknown.png'
           alt="sprite"
           height="50px"
-          width="55px"
+          width="50px"
         //   style={{left: (calc(percentComplete - 55)) + '%'}}
-        style={{left: dynamicPosition}}
+        style={{left: dynamicPosition, borderRadius: '50%'}}
         />
       <div className="track">
         <div className="start"></div>
