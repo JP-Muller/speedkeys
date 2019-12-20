047bdbb00da2c23c3c350ddd47879c09a394f572
Jake M <mullerjp95@gmail.com>
2019-12-20 18:17:29 -0500

    server functional, DB connected

---
+++ server/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config({path: __dirname + '/../.env'})
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+
+//require sockets
+const socket = require('socket.io')
+const ac = require('./controllers/authController')
+const initSession = require('./middleware/initSession')
+const authCheck = require('./middleware/authCheck')
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
+
+const app = express()
+
+//used in the socket
+const io = socket(app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`)))
+
+app.use(express.json())
+
+app.use(session({
+    secret: SESSION_SECRET,
+    saveUninitialized: true,
+    resave: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 365}
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    console.log('DB Connected')
+})
+
+app.use(initSession)
+
+// Authentication Endpoints
+
+app.post('/api/login')
+app.post('/api/signup')
+app.get('/api/user', authCheck)
+app.delete('/api/logout')
+app.put('/api/edit/:user_id')
+app.put('/api/editprofile_pic/:user_id')
