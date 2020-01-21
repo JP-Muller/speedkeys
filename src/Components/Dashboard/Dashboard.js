66a7566a4326a271cd18575dd28a8ee8025ca9b5
Jake M <mullerjp95@gmail.com>
2020-01-20 22:39:55 -0500

    created leaderboard component

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -4,7 +4,9 @@ import "./Dashboard.css";
 function Dashboard() {
   return (
     <div className="dash-master">
-      <aside className='ad-column'>Ad Banner</aside>
+      <aside className='ad-column'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR08APsAnQxOyiWsxuOmKGFDkXJ06FXCxI4iFk9wnweozoRHa2u&s' alt='ad-column' />
+      </aside>
       <main className='main-content'>
       <header className='mt-3 mb-4'>
         <h1>SpeedKeys - Test your speed!</h1>
@@ -20,7 +22,9 @@ function Dashboard() {
         <p>Join other players and compete online!</p>
       </div>
       </main>
-      <aside className='ad-column'>Banner ad</aside>
+      <aside className='ad-column'>
+        <img src='https://webcdn-adespressoinc.netdna-ssl.com/wp-content/uploads/2018/07/free-trial-planet-2-med.jpg' alt='ad-column' />
+      </aside>
     </div>
   );
 }
